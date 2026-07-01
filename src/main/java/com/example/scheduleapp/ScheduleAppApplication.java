24a2ec53d5c5f169ee64531de49ca61472935ab6
b7749abc92-boop <b7749abc92@gmail.com>
2026-07-01 15:16:06 +0900

    [일정 관리 앱 만들기] 과제 제작진행

--- src/main/java/com/example/scheduleapp/ScheduleAppApplication.java
+++ src/main/java/com/example/scheduleapp/ScheduleAppApplication.java
@@ -1,9 +1,12 @@
 package com.example.scheduleapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScheduleAppApplication {
 
     public static void main(String[] args) {
